e156b788cc23382771e3593db80940d5d9ef3662
Rahul Rakshit <rahulrakshit@gmx.com>
2018-05-21 15:51:16 +0100

    complete challenge #03: debugging

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,13 @@
+require "oystercard"
+
 describe Oystercard do
+
+  context "#balance" do
+
+    it "should respond to the message 'balance'" do
+      expect(subject.balance).to eq 0
+    end
+
+  end
+
 end
