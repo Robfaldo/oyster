f9da601e5340634e89e3cecb9d9292142817bd7b
Robert Faldo <robertfaldo@gmail.com>
2018-05-21 16:04:35 +0100

    challenge #05: top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,14 @@ require "oystercard"
 
 describe Oystercard do
 
-  context "#balance" do
+  context "#balance and top up" do
 
-    it "should respond to the message 'balance'" do
+    it "should be initialised with balance of 0" do
       expect(subject.balance).to eq 0
+    end
+
+    it '#top_up - add money to balance' do
+      expect { subject.top_up 10 }.to change { subject.balance }.by 10
     end
 
   end
